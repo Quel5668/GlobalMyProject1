9c759934ae798c3b87a469cc8b62aaa3f35cc193
Anton Ilin <anton.stu@mail.ru>
2018-11-13 01:16:00 +0300

    Hi

--- funcs.cpp
+++ funcs.cpp
@@ -18,18 +18,9 @@ double MyDiv(double a, double b)
 {
 return a / b;
 }
-<<<<<<< HEAD
-int MyPow(int num, int n) {
-int res = num;
-for (int i = 1; i < n; i++) {
-res *= num;
-}
-return res;
-}
-=======
 double MyPow(double a, double b)
 {
 return exp(b * log(a));
 }
 
->>>>>>> develop
+
--- funcs.h
+++ funcs.h
@@ -1,3 +1,4 @@
+//Hi
 // заголовочный файл funcs.h
 #ifndef __FUNCS_H__
 #define __FUNCS_H__
@@ -5,11 +6,7 @@ double MySum(double, double);
 double MySub(double, double);
 double MyMul(double, double);
 double MyDiv(double, double);
-<<<<<<< HEAD
-int MyPow(int, int);
-=======
 double MyPow(double, double);
 
->>>>>>> develop
 
 #endif // __FUNCS_H__
